e73c9f0f2a551505a7168f8c76bef4bc2fecfa3c
matonism <michaelmatonis@hotmail.com>
2015-06-01 19:06:26 -0700

    displays an extra number

--- FirstProject/main/FirstPage.java
+++ FirstProject/main/FirstPage.java
@@ -5,7 +5,7 @@ public class FirstPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 6;
-		while (i < 10){
+		while (i < 17){
 			System.out.println(i);
 			i++;
 		}
